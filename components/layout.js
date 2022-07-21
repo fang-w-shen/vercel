928c6c0920e8e21f9bf35865be6a91105760ddc4
Fang <fang-wen.shen@vanderbilt.edu>
2022-07-21 05:49:57 -0500

    Dev

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
 import { useEffect } from "react";
-import utilStyles from "../styles/utils.module.css";
-import styles from "./layout.module.css";
 
 const name = `👋  I'm Fang`;
 export const siteTitle = "FWS - Home";
@@ -17,106 +12,6 @@ export default function Layout({ children, home }) {
 
   })
   return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="FWS - Portfolio" />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-      </Head>
-      <header className={styles.header + ` ${home ? styles.fullScreen : ""}`}>
-        {home ? (
-          <>
-
-            <button id="flip">
-              <div id="card">
-                <div className="front">
-                  <Image
-                    priority
-                    src="/images/icon.svg"
-                    className={utilStyles.borderCircle}
-                    height={144}
-                    width={144}
-                    alt={name}
-                  />
-                </div>
-
-                <div className="back">
-                  <Image
-                    priority
-                    src="/images/profile.svg"
-                    height={144}
-                    width={144}
-                    alt={name}
-                  />
-                </div>
-
-              </div>
-            </button>
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-            <button className="arrow bounce"></button>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {
-        !home && (
-          <div className={styles.backToHome}>
-            <Link href="/">
-              <a>← Back to home</a>
-            </Link>
-          </div>
-        )
-      }
-
-      <footer>
-        <div>
-          <a href="https://linkedin.com/in/fang-wen-shen">
-            <Image
-              priority
-              src="/images/linkedin.svg"
-              height={33}
-              width={33}
-              alt={name}
-            />
-          </a>
-          <a href="https://github.com/fang-w-shen">
-            <Image
-              priority
-              src="/images/github.svg"
-              height={33}
-              width={33}
-              alt={name}
-            />
-          </a>
-        </div>
-        © {new Date().getFullYear()} <a href='https://github.com/fang-w-shen'>Fang-Wen Shen</a>
-      </footer>
-    </div >
+    <div></div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,13 @@
-import Head from "next/head";
-import Link from "next/link";
-import { useEffect } from "react";
-import MonthDate from "../components/date";
-import Layout, { siteTitle } from "../components/layout";
-import { getSortedPostsData } from "../lib/projects";
-import utilStyles from "../styles/utils.module.css";
-import MovingStars from '../utils/MovingStars';
+import Layout from "../components/layout";
 
-export default function Home({ projects }) {
-  useEffect(() => {
-    MovingStars()
-  }, [])
+export default function Home() {
+
 
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.fullWidth}`}>
-        <canvas id='canvas3'></canvas>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Projects</h2>
-        <ul className={utilStyles.list}>
-          {projects.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/portfolio/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <MonthDate dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+     
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const projects = getSortedPostsData();
-  return {
-    props: {
-      projects,
-    },
-  };
-}
+
